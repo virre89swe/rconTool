d9ed9a1f8f2a5f9765c70a18e76eb45285315286
virre <calleviktor@telia.com>
2013-09-28 13:06:43 +0200

    File removal test

--- src/com/rcon/idtech3/view/SettingsActionListener.java
+++
@@ -1,9 +0,0 @@
-package com.rcon.idtech3.view;
-
-public interface SettingsActionListener
-{
-	public boolean SaveAction(SettingsActionEvent event);
-	
-	public String TestConnAction(SettingsActionEvent event);
-	
-}
